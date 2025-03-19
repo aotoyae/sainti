50d97ef89c01b48ada51a97e8678ab34f863a7f0
kimsohyeon <aotoyae@gmail.com>
2025-03-19 14:38:53 +0900

    style: login page CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center">
+    <main className="min-h-screen flex flex-col justify-center items-center gap-20">
       <h1 className="text-2xl text-blue-900 font-bold">Login Page</h1>
-      <form className="flex flex-col gap-6">
+      <form className="w-[400px] flex flex-col gap-6 text-sm">
         <div className="flex flex-col gap-1">
           <label htmlFor="">아이디</label>
-          <input id="id" type="text" placeholder="아이디를 입력하세요" />
+          <input
+            id="id"
+            type="text"
+            placeholder="아이디를 입력하세요"
+            className="p-2 border border-solid border-gray-300 rounded-md"
+          />
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="">패스워드</label>
-          <input id="pw" type="password" placeholder="패스워드를 입력하세요" />
+          <input
+            id="pw"
+            type="password"
+            placeholder="패스워드를 입력하세요"
+            className="p-2 border border-solid border-gray-300 rounded-md"
+          />
         </div>
-        <button>Log in</button>
+        <button className="py-3 bg-blue-900 text-white rounded-full">
+          Log in
+        </button>
       </form>
     </main>
   );
