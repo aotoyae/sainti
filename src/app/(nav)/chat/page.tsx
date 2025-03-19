67d387238b8752dd3e90be7e2716f0662e75451f
kimsohyeon <aotoyae@gmail.com>
2025-03-19 15:31:15 +0900

    feat: mail, chat page 화면 구성

--- src/app/(nav)/chat/page.tsx
+++ src/app/(nav)/chat/page.tsx
@@ -1,5 +1,9 @@
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  return (
+    <div className="h-full flex justify-center items-center">
+      <h1 className="text-2xl font-black">채팅 페이지입니다.</h1>
+    </div>
+  );
 };
 
 export default ChatPage;
