b47d860e721e1f4bf19941a7c154446cc4a3411d
kimsohyeon <aotoyae@gmail.com>
2025-03-19 14:15:32 +0900

    feat: login page form 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <main>sainti</main>;
+  return (
+    <main className="min-h-screen flex flex-col justify-center items-center">
+      <h1>Login Page</h1>
+      <form className="flex flex-col gap-6">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">아이디</label>
+          <input id="id" type="text" placeholder="아이디를 입력하세요" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="">패스워드</label>
+          <input id="pw" type="password" placeholder="패스워드를 입력하세요" />
+        </div>
+        <button>Log in</button>
+      </form>
+    </main>
+  );
 }
